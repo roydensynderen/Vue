4ece8e42e200bc118e7593d87203299cfe386a89
Royden Shin Tan <roydensynderen@gmail.com>
2018-10-15 00:12:25 -0700

    Complain form completed

--- complain-form/src/App.vue
+++ complain-form/src/App.vue
@@ -97,17 +97,22 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+              <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <appSwitch v-model="dataSwitch"></appSwitch>
+              </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <button
+                    <button @click.prevent="submitted"
                             class="btn btn-primary">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -124,7 +129,7 @@
                         </ul>
                         <p>Gender: {{ gender }}</p>
                         <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -133,6 +138,8 @@
 </template>
 
 <script>
+import Switch from './components/Switch';
+
 export default {
   data() {
     return {
@@ -145,8 +152,18 @@ export default {
       sendMail: [],
       gender: 'Male',
       selectedPriority: 'Low',
-      priorities: ['High', 'Medium', 'Low']
+      priorities: ['High', 'Medium', 'Low'],
+      dataSwitch: true,
+      isSubmitted: false
     }
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    }
+  },
+  components: {
+    appSwitch: Switch
   }
 }
 </script>
