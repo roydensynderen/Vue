bdecb6d17247a64d699ce4ceea675ab72900cace
Royden Shin Tan <roydensynderen@gmail.com>
2018-10-16 19:02:59 -0700

    Animations and transitions in Vue

---
+++ animations/src/App.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <button class='btn btn-primary' @click='show = !show'>Show Info</button>
+                <br><br/>
+                <transition name='fade'>
+                  <div class='alert alert-info' v-if='show'>This is some information</div>
+                </transition>
+                <!-- type = 'animation' allows animation to finish first -->
+                <transition name='slide' type='animation'>
+                  <div class='alert alert-info' v-if='show'>This is some information</div>
+                </transition>
+
+                <transition name='fade' appear>
+                  <div class='alert alert-info' v-if='!show'>This is some information</div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+              show: false
+            }
+        }
+    }
+</script>
+
+<style>
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+    /* opacity: 1; */
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  .slide-enter {
+    opacity: 0;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity 1s;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 3s;
+    opacity: 0;  
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+    @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
+</style>
