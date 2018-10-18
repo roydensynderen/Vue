aa818cdb583384facd25e0f3b429f3269a81b796
Royden Shin Tan <roydensynderen@gmail.com>
2018-10-18 00:24:23 -0700

    Exploring vue animations/transitions

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -4,18 +4,50 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select v-model='alertAnimation' class='form-control'>
+                  <option value='fade'>Fade</option>
+                  <option value='slide'>Slide</option>
+                </select>
+                <br><br/>
                 <button class='btn btn-primary' @click='show = !show'>Show Info</button>
                 <br><br/>
-                <transition name='fade'>
+                <transition :name='alertAnimation'>
                   <div class='alert alert-info' v-if='show'>This is some information</div>
                 </transition>
                 <!-- type = 'animation' allows animation to finish first -->
-                <transition name='slide' type='animation'>
+                <transition name='slide' type='animation' appear>
                   <div class='alert alert-info' v-if='show'>This is some information</div>
                 </transition>
 
-                <transition name='fade' appear>
-                  <div class='alert alert-info' v-if='!show'>This is some information</div>
+                <transition enter-active-class='animated bounce'
+                            leave-active-class='animated shake'>
+                  <div class='alert alert-info' v-if='show'>This is some information</div>
+                </transition>
+                <transition :name='alertAnimation' mode='out-in'>
+                  <div class='alert alert-info' v-if='show' key='info'>This is some information</div>
+                  <div class='alert alert-warning' v-else key='warning'>This is some warning</div>
+                </transition>
+                <hr>
+                <button class='btn btn-primary' @click='load = !load'>Load / Remove Element</button>
+                <br><br>
+                <transition @before-enter='beforeEnter'
+                            @enter='enter'
+                            @fter-enter='afterEnter'
+                            @enter-cancelled='enterCancelled'
+                            
+                            @before-leave='beforeLeave'
+                            @leave='leave'
+                            @after-leave='afterLeave'
+                            @leave-cancelled='leaveCancelled'
+
+                            :css='false'>
+                  <div style='width: 300px; height: 100px; background-color: lightgreen;' v-if='load'></div>
+                </transition>
+                <hr>
+                <button class='btn btn-primary' @click='selectedComponent == "appSuccessAlert" ? selectedComponent = "appDangerAlert" : selectedComponent = "appSuccessAlert"'>Toggler</button>
+                <br><br>
+                <transition name='fade' mode='out-in'>
+                  <component :is='selectedComponent'></component>
                 </transition>
             </div>
         </div>
@@ -23,11 +55,54 @@
 </template>
 
 <script>
+import DangerAlert from './components/DangerAlert';
+import SuccessAlert from './components/SuccessAlert';
+
     export default {
         data() {
             return {
-              show: false
+              show: false,
+              load: true,
+              alertAnimation: 'fade',
+              elementWidth: 100,
+              selectedComponent: 'appSuccessAlert'
             }
+        },
+        methods: {
+          beforeEnter(el) {
+            this.elementWidth = 100;
+            el.style.width = this.elementWidth + 'px'
+          },
+          enter(el, done) {
+            let round = 1;
+            const interval = setInterval(() => {
+              el.style.width = (this.elementWidth + round * 10) + 'px';
+              round++;
+              if (round > 20) {
+                clearInterval(interval);
+                done();
+              }
+            }, 20);
+          },
+          beforeLeave(el) {
+            this.elementWidth = 300;
+            el.style.width = this.elementWidth + 'px';
+          },
+          leave(el, done) {
+            let round = 1;
+            const interval = setInterval(() => {
+              el.style.width = (this.elementWidth - round * 10) + 'px';
+              round++;
+              if (round > 20) {
+                clearInterval(interval);
+                done();
+              }
+            }, 20);
+          }
+        },
+        components: {
+          appDangerAlert: DangerAlert,
+          appSuccessAlert: SuccessAlert
         }
     }
 </script>
