0f3857837c735c26098fbdd8b420e769305d01ac
Royden Shin Tan <roydensynderen@gmail.com>
2018-10-23 16:10:19 -0700

    introduction to Vue routing

---
+++ vue-routing/src/routes.js
@@ -0,0 +1,7 @@
+import User from './components/User/User.vue';
+import Home from './components/User/User.vue';
+
+export const routes = [
+    { path: '/user', component: User },
+    { path: '', component: Home }
+];
