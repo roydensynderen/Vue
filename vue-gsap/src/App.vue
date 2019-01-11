9f86fb8930f4339e80fb27343c8f83de59baa922
Royden <roydensynderen@gmail.com>
2019-01-11 18:33:38 -0500

    Vue x GSAP

---
+++ vue-gsap/src/App.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  .box(ref="box")
+</template>
+
+<script>
+import { TimelineLite, Back } from 'gsap'
+
+export default {
+  mounted() {
+    const { box } = this.$refs
+    const timeline = new TimelineLite()
+
+    timeline.to(box, 1, { 
+      x: 200, 
+      rotation: 90,
+      ease: Back.easeInOut
+    })
+
+    timeline.to(box, 0.5, {
+      background: 'green'
+    }, '-=0.5')
+  }
+}
+</script>
+
+<style lang="sass">
+*
+  box-sizing: border-box
+  margin: 0
+  padding: 0
+
+.box
+  height: 60px
+  width: 60px
+  background: red
+</style>
+
---
+++ vue-slack-bubble/src/App.vue
@@ -0,0 +1,90 @@
+<template lang="pug">
+  .bubble-wrapper
+    .bubble( ref="bubble")
+      img.bubble-image( src="https://s3.ap-southeast-2.amazonaws.com/daily-fire-assets/slack-white.svg" )
+    .bubble-pulse( ref="bubblePulse" )
+</template>
+
+<script>
+import { TimelineLite, Back, Elastic, Expo } from 'gsap'
+export default {
+  data() {
+    return {
+      timeline: null
+    };
+  },
+  mounted() {
+    const { bubble, bubblePulse } = this.$refs;
+    this.timeline = new TimelineLite({
+      onComplete: () => this.timeline.restart()
+    });
+    this.timeline.to(bubble, 0.4, {
+      scale: 0.8,
+      rotation: 16,
+      ease: Back.easeOut.config(1.7)
+    });
+    this.timeline.to(
+      bubblePulse,
+      0.8,
+      {
+        scale: 0.9,
+        opacity: 1
+      },
+      "-=0.6"
+    );
+    this.timeline.to(bubble, 1.2, {
+      scale: 1,
+      rotation: "-=16",
+      ease: Elastic.easeOut.config(2.5, 0.5)
+    });
+    this.timeline.to(
+      bubblePulse,
+      1.1,
+      {
+        scale: 3,
+        opacity: 0,
+        ease: Expo.easeOut
+      },
+      "-=1.2"
+    );
+  }
+}
+</script>
+
+<style lang="sass">
+body
+  display: flex
+  align-items: center
+  justify-content: center
+  background: #1a1a1a
+  padding-top: 100px
+
+.bubble
+  position: relative
+  display: flex
+  align-items: center
+  justify-content: center
+  border: 1px solid white
+  background: #272727
+  border-radius: 50%
+  height: 100px
+  width: 100px
+    z-index: 2
+  &-wrapper
+    position: relative
+  &-image
+    height: 50%
+  &-pulse
+    position: absolute
+    z-index: 1
+    height: 120px
+    width: 120px
+    top: 50%
+    left: 50%
+    margin-top: -60px
+    margin-left: -60px
+    background: #272727
+    border-radius: 50%
+    opacity: 0
+    transform: scale(0)
+</style>
